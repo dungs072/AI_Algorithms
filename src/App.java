9f09a096f22d9593edfd77e845ea3420421dba24
Your Name <82323846+dungs072@users.noreply.github.com>
2023-01-31 00:18:15 +0700

    add DFS

--- src/App.java
+++ src/App.java
@@ -6,24 +6,31 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 import PathFinding.Edge;
 import PathFinding.EdgeList;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    //initial
+    // initial
     EdgeList edgeList = new EdgeList();
-    HashMap<String,String> visitedHashList = new HashMap<String,String>();
+    HashMap<String, String> visitedHashList = new HashMap<String, String>();
     loadDataFromFile(edgeList);
     edgeList.SortEdgeList();
-    String startV = "D";
+    String startV = "F";
     String desV = "B";
 
-    //implement FBS
+    System.out.println("Implement BFS");
     if(FBS(startV,desV, edgeList,visitedHashList))
     {
+    displayPath(startV, desV, visitedHashList);
+    visitedHashList.clear();
+    }
+    System.out.println("\nImplement DFS");
+    if (DFS(startV, desV, edgeList, visitedHashList)) {
       displayPath(startV, desV, visitedHashList);
+      visitedHashList.clear();
     }
   }
 
@@ -33,7 +40,7 @@ public class App {
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        data = data.substring(1, data.length()-1);
+        data = data.substring(1, data.length() - 1);
         String[] texts = data.split(",");
         edgeList.AddEdge(texts[0], texts[1], Integer.parseInt(texts[2]));
       }
@@ -43,54 +50,77 @@ public class App {
       e.printStackTrace();
     }
   }
-  private static void displayPath(String startV,String desV,HashMap<String,String> visitedHashList)
-  {
-    ArrayList<String> paths=  new ArrayList<>();
+
+  private static void displayPath(String startV, String desV, HashMap<String, String> visitedHashList) {
+    ArrayList<String> paths = new ArrayList<>();
     String parent = visitedHashList.get(desV);
     paths.add(desV);
-    while(!startV.equals(parent))
-    {
+    while (!startV.equals(parent)) {
       paths.add(parent);
-      parent = visitedHashList.get(parent); 
+      parent = visitedHashList.get(parent);
     }
     paths.add(startV);
     Collections.reverse(paths);
-    for(String v: paths)
-    {
-      System.out.print(v+" ");
+    for (String v : paths) {
+      System.out.print(v + " ");
     }
-    
-    
+
   }
-  private static boolean FBS(String startV, String desV, EdgeList edgeList,HashMap<String,String> visitedHashList)
-  {
-      if(!edgeList.checkHasVertice(startV)||!edgeList.checkHasVertice(desV))
-      {
-          System.out.println("Cannot find path !!!!!");
-          return false;
-      }
-      Queue<String> queue = new LinkedList<>();
-      queue.add(startV);
-      while(!queue.isEmpty())
-      {
-        String v = queue.poll();
-        
-        for(Edge e:edgeList.getEdges())
-        {
-            if(v.equals(e.getStartVertices()))
-            {
-              if(visitedHashList.containsKey(e.getEndVertices())){continue;}
-              queue.add(e.getEndVertices());
-              visitedHashList.put(e.getEndVertices(), v);
-              if(e.getEndVertices().equals(desV))
-              {
-                return true;
-              }
-            }
+
+  private static boolean FBS(String startV, String desV, EdgeList edgeList, HashMap<String, String> visitedHashList) {
+    if(startV.equals(desV)){return false;}
+    if (!edgeList.checkHasVertice(startV) || !edgeList.checkHasVertice(desV)) {
+      System.out.println("Cannot find path !!!!!");
+      return false;
+    }
+    Queue<String> queue = new LinkedList<>();
+    queue.add(startV);
+    while (!queue.isEmpty()) {
+      String v = queue.poll();
+
+      for (Edge e : edgeList.getEdges()) {
+        if (v.equals(e.getStartVertices())) {
+          if (visitedHashList.containsKey(e.getEndVertices())) {
+            continue;
+          }
+          queue.add(e.getEndVertices());
+          visitedHashList.put(e.getEndVertices(), v);
+          if (e.getEndVertices().equals(desV)) {
+            return true;
+          }
         }
       }
+    }
+    System.out.println("Cannot find path !!!!!");
+
+    return false;
+  }
+
+  private static boolean DFS(String startV, String desV, EdgeList edgeList, HashMap<String, String> visitedHashList) {
+    if(startV.equals(desV)){return false;}
+    if (!edgeList.checkHasVertice(startV) || !edgeList.checkHasVertice(desV)) {
       System.out.println("Cannot find path !!!!!");
-      
       return false;
+    }
+    Stack<String> stack = new Stack<>();
+    stack.push(startV);
+    while (!stack.isEmpty()) {
+      String v = stack.pop();
+      for (Edge e : edgeList.getEdges()) {
+        if (v.equals(e.getStartVertices())) {
+          if (visitedHashList.containsKey(e.getEndVertices())) {
+            continue;
+          }
+          stack.push(e.getEndVertices());
+          visitedHashList.put(e.getEndVertices(), v);
+          if (e.getEndVertices().equals(desV)) {
+            return true;
+          }
+        }
+      }
+    }
+    System.out.println("Cannot find path !!!!!");
+
+    return false;
   }
 }
